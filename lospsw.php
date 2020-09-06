ad27ba7ca2254cce2939ad825aa5dc2791671d2f
osmanozz <2120601@talnet.nl>
2020-09-06 13:39:51 +0200

    Week 1 les 2 forms

---
+++ lospsw.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+
+  </head>
+  <body>
+    <form action="signup.php" method="get">
+E-mail <input type="text" name="e-mail"><br>
+<input type="button" value="Send">
+
+</form>
+  </body>
+</html>
+
+<?php
+
+ ?>
