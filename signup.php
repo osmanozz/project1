ae81036159d42c4ac0d77c469250b5a5781aecd4
osmanozz <2120601@talnet.nl>
2020-09-06 13:40:38 +0200

    Week 1 les 2 forms

---
+++ signup.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="signup.php" method="get">
+Voornaam* <input type="username" name="username"><br>
+Tussenvoegsel <input type="password" name="password"><br>
+Achternaam* <input type="password" name="password"><br>
+E-mail* <input type="password" name="password"><br>
+Gebruikersnaam* <input type="password" name="password"><br>
+Wachtwoord* <input type="password" name="password"><br>
+Herhaal Wachtwoord* <input type="password" name="password"><br>
+(De velden met * zijn verplicht) <br />
+<input type="button" value="Word gebruiker">
+
+</form>
+  </body>
+</html>
+
+<?php
+
+ ?>
