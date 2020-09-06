d0998bb9f723b29e59a21a919c0c20b2c7fbfd49
osmanozz <2120601@talnet.nl>
2020-09-06 13:38:17 +0200

    Week 1 les 2 forms

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head> 
+  <body>
+    <form action="index.php" method="get">
+Username <input type="username" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit" value="Log in">
+<br>
+<a href="lospsw.php">Wachtwoord vergeten</a>
+<br>
+<a href="signup.php">Nieuw account</a>
+
+</form>
+  </body>
+</html>
+
+<?php
+
+ ?>
