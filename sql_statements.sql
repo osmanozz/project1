8a673e98e3a6ccdd1b4149889d3dd849ea410b7b
osmanozz <2120601@talnet.nl>
2020-09-09 10:18:20 +0200

    sql file

---
+++ sql_statements.sql
@@ -0,0 +1,17 @@
+CREATE TABLE account(
+id int NOT null AUTO_INCREMENT,
+email varchar(255),
+wachtwoord varchar(255),
+PRIMARY KEY(id)
+);
+CREATE TABLE persoon( 
+id int AUTO_INCREMENT not null, 
+Voornaam varchar(255), 
+Tussenvoegsel varchar(255), 
+Achternaam varchar(255), 
+Gebruikersnaam varchar(255), 
+account_id int, PRIMARY KEY(id), 
+FOREIGN KEY(account_id) REFERENCES account(id) 
+);
+
+
