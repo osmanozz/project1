16fb9b341ebc1a49f20dafaffa27e0cef7f3a077
osmanozz <2120601@talnet.nl>
2020-09-18 17:03:44 +0200

    versie 1.2

--- index.php
+++ index.php
@@ -3,17 +3,19 @@
 
   </head>
       <body>
-        <form action="index.php" method="get">
-        Username <input type="username" name="username" required><br>
-        Password: <input type="password" name="password" required><br>
-        <input type="submit" value="Log in">
-            <br>
-          <a href="lospsw.php">Wachtwoord vergeten</a>
-            <br>  
-          <a href="signup.php">Nieuw account</a>
-
-    </form>
-  </body>
+        <div id="parent">
+          <link rel="stylesheet" href="style.css">
+              <form id="form" action="index.php" method="get">
+                Username: <input type="username" name="username" required><br>
+                Password: <input type="password" name="password" required><br>
+                  <input class="knop" type="submit" value="Log in">
+                    <br>
+                        <a href="lospsw.php">Wachtwoord vergeten</a>
+                    <br>
+                        <a href="signup.php">Nieuw account</a>
+          </div>
+             </form>
+      </body>
 </html>
 
 <?php
